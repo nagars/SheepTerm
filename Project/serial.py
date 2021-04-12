29aeb96ad3360366df5b8c5d785af3926ed5d06f
SHAWN-PC\Shawn Nagar <shawnnagar@gmail.com>
2021-04-12 20:31:22 +0530

    Implemented UI design as Flowchart. Blank window implemented in code.

---
+++ Project/serial.py
@@ -0,0 +1,13 @@
+import tkinter                       # Import tkinter library used to generate GUI
+from tkinter import *                # Import tkinter modules used to generate GUI
+from tkinter import scrolledtext     # Import tkinter module for scroll text box
+
+# Generate GUI window
+window = Tk()
+# Define window size
+window.geometry('750x500')
+# Set title for window
+window.title("Shawn's Serial Terminal")
+
+#Main loop
+window.mainloop()
