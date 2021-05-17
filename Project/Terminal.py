4c3cacc342d44796ca48c77890624798337a946b
SHAWN-PC\Shawn Nagar <shawnnagar@gmail.com>
2021-05-17 20:26:42 +0530

    Updated UI design to include boxes to change communication settings. New UI impllemented using multiple frames instead of one. Incomplete.

--- Project/Terminal.py
+++ Project/Terminal.py
@@ -3,10 +3,9 @@ from tkinter import *                # Import tkinter modules used to generate G
 from tkinter import scrolledtext     # Import tkinter module for scroll text box
 from tkinter import ttk
 
-import serial
-from serial.serialutil import STOPBITS_ONE_POINT_FIVE                       # Import pyserial library
-# Import function to list serial ports.
-import serial.tools.list_ports as port_list
+import serial  # Import pyserial library
+from serial.serialutil import STOPBITS_ONE_POINT_FIVE       
+import serial.tools.list_ports as port_list # Import function to list serial ports.
 from serial import SerialException  # Import pyserial exception handling
 
 '''
@@ -71,6 +70,57 @@ def send_button_pressed():
 
     return
 
+'''Functions: Frame Definitions'''
+def define_frame(container, position_y, position_x):
+
+    frame = Frame(container)
+    frame.grid(column=position_y, row=position_x, sticky=(N, W, E, S))
+
+    return frame
+
+'''Functions: Widget Definitions'''
+def define_drop_down(container, content_list, default_state, position_y, position_x):
+
+    menu = ttk.Combobox(container, value=content_list, state = default_state)
+    menu.grid(column=position_y, row=position_x)
+    menu.current()
+
+    return menu
+
+def define_button(container, text, default_state, function_call, position_y, position_x):
+    
+    button = ttk.Button(container, text=text,
+                        command=function_call, state=default_state)
+    button.grid(column=position_y, row=position_x)
+
+    return
+
+def define_label(container, text, position_y, position_x):
+
+    label = Label(container, text=text)
+    label.grid(column=0,row=0)
+
+    return label
+
+def define_checkbox(container, text, default_state, status_variable, position_y, posiion_x):
+
+    checkbox = Checkbutton(container, text=text, variable=status_variable, state=default_state)
+    checkbox.grid(column=position_y,row=posiion_x)
+
+    return checkbox
+
+def define_scroll_textbox(container, width, height, position_y, position_x):
+
+    scrollbox = scrolledtext.ScrolledText(container, width=width, height=height)
+    scrollbox.grid(column=position_y, row=position_x)
+
+    return scrollbox
+
+def define_entry_textbox(container, width, default_state, position_y, position_x):
+    entrybox = Entry(container, width=width, state=default_state)
+    entrybox.grid(column=position_y, row=position_x)
+
+    return entrybox
 
 '''
 Definitions
@@ -81,79 +131,63 @@ window = Tk()
 window.geometry('750x500')
 # Set title for window
 window.title("Shawn's Serial Terminal")
-
-# Define a frame
-main_frame = Frame(window)
-main_frame.grid(column=0, row=0, sticky=(N, W, E, S))
-main_frame.columnconfigure(0, weight=4)
-main_frame.rowconfigure(0, weight=4)
+#Configure its layout
+window.columnconfigure(0,weight=0)
+window.rowconfigure(0,weight=0)
 
 # Variable Definitions
 echo_flag = tkinter.BooleanVar()    # Checks if echo checkbox is set/cleared
+timestamp_flag = tkinter.BooleanVar() 
 include_null_flag = tkinter.BooleanVar()
 include_line_flag = tkinter.BooleanVar()
 
-# Define Widgets #
-
+# Create COM Frame #
+com_frame = define_frame(window,0,0)
 # Define a label for com port to be placed near text box
-com_port_label = Label(main_frame, text="COM PORT")
-com_port_label.grid(column=0, row=0)
-# Set its position in top left corner
-#com_port_label.pack()
-
-# Define Drop down Menu for COM ports
+com_port_label = define_label(com_frame, "COM PORT", 0, 0)
 # List all the available serial ports
 com_ports_available = list(port_list.comports())
-
 # Define the drop down menu for com ports
-com_ports_menu = ttk.Combobox(main_frame, value=com_ports_available, state = 'readonly')
-com_ports_menu.grid(column=1, row=0)
-#com_ports_menu.pack()
-com_ports_menu.current()
-
+com_ports_menu = define_drop_down(com_frame, com_ports_available, 'readonly', 1, 0)
 # Define Set COM port button
-open_com_button = ttk.Button(main_frame, text="Open Port",
-                             command=open_com_port)
-open_com_button.grid(column=2, row=0)
-#open_com_button.pack()
+open_com_button = define_button(com_frame, "Open Port", 'normal',
+                                open_com_port,2,0),
 
-# Create a scroll text box for the terminal
-terminal_box = scrolledtext.ScrolledText(main_frame, width=47, height=20)
-terminal_box.grid(column=1,row=1)
-#terminal_box.pack()
 
-# Create a text box to get the transmit message from user
-send_message_textbox = Entry(main_frame, width=10, state='disabled')
-send_message_textbox.grid(column=1,row=2)
-#send_message_textbox.pack()
-
-# Create a button to send data on selected port
-send_button = Button(main_frame, text="Send",
-                     command=send_button_pressed, state='disabled')
-send_button.grid(column=2,row=2)
-#send_button.pack()
-
+# Create Display Configure Frame #
+config_frame = define_frame(window, 0, 1)
+# Define a label for data type drop down
+display_type_label = define_label(config_frame, "Display Type", 0, 0)
+display_type_label.grid(sticky=W)
 #Create an echo to terminal checkbox
-echo_checkbox = Checkbutton(main_frame, text="Enable Echo", variable=echo_flag)
-echo_checkbox.grid(column=0,row=2)
-#echo_checkbox.pack()
-
+echo_checkbox = define_checkbox(config_frame, "Enable Echo", "normal", echo_flag, 0, 1)
+echo_checkbox.grid(sticky=W)
+#Create a show timestamp checkbox
+timestamp_checkbox = define_checkbox(config_frame, "Show Timestamp", 
+                        "normal", timestamp_flag, 0, 2)
+timestamp_checkbox.grid(sticky=W)
 #Generate list of data types
 data_types = ["STRING","ASCII","HEX"]
-
 #Create a drop down menu with different datatypes to represent on terminal
-data_types_menu = ttk.Combobox(main_frame, textvariable=data_types[0], values=data_types, state = 'readonly')
-data_types_menu.grid(column=0,row=1)
-#data_types_menu.pack()
-data_types_menu.current()
-
+data_types_menu = define_drop_down(config_frame, data_types, 'readonly',1,0)
 #Create an include null character checkbox
-include_null_checkbox = Checkbutton(main_frame, text="Include Null Character", variable=include_null_flag)
-include_null_checkbox.grid(column=0,row=3)
-
+include_null_checkbox = define_checkbox(config_frame, "Include Null Character", "normal", 
+                                        include_null_flag, 0, 3)
 #Create an include next line character checkbox
-include_line_checkbox = Checkbutton(main_frame, text="Include Line Character", variable=include_line_flag)
-include_line_checkbox.grid(column=0,row=4)
+include_line_checkbox = define_checkbox(config_frame, "Include Line Character", "normal",
+                                        include_line_flag, 0, 4)
+
+
+
+# Create a text box to get the transmit message from user
+send_message_textbox = define_entry_textbox(window, 10, 'disabled', 1, 2)
+
+# Create a button to send data on selected port
+send_button = define_button(window, "Send", 'disabled',
+                     send_button_pressed, 2, 2)
+
+# Create a scroll text box for the terminal
+terminal_box = define_scroll_textbox(window, 47, 20, 1, 1)
 
 # Main loop
 window.mainloop()
